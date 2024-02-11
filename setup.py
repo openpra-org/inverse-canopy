1c0d4744a2ef08312c8cfa6423ef5084255a75cb
Arjun Earthperson <aarjun@ncsu.edu>
2024-02-11 01:23:03 +0000

    docs fixes - fixed packaging issues - fixed docs and demo code - bumped to v0.0.5
    Co-authored-by: Arjun Earthperson <mail@earthperson.org>
    
    Merge-request: OPENPRA-MR-62
    Merged-by: Mihai Diaconeasa <madiacon@ncsu.edu>

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open('README.md') as f:
 
 setup(
     name='inverse-canopy',
-    url='https://git@git.space.openpra.org/openpra/inverse-canopy.git',
-    version='0.0.4',
+    url='https://openpra.org',
+    version='0.0.5',
     author='Arjun Earthperson',
     author_email='arjun@openpra.org',
-    license='AGPL3',
+    license='AGPL-3.0',
     description='',
+    long_description_content_type='text/markdown',
     long_description=README,
     install_requires=[
         'tensorflow==2.15.0',
@@ -22,9 +23,10 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Programming Language :: Python :: 3.8',
     ],
+    packages=['inverse_canopy'],
     python_requires='>=3.6',
     entry_points={
     },

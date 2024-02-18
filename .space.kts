91877473f2d3f072df6fb65e0c1d7d78e665e002
Arjun Earthperson <aarjun@ncsu.edu>
2024-02-18 12:13:02 -0500

    - fixed tests

--- .space.kts
+++ .space.kts
@@ -53,7 +53,7 @@ job("Package") {
       shellScript("lint"){
         interpreter = "/bin/bash"
         content = """
-                          docker run --rm "$remote:{{ branchSlug }}" pylint
+                          docker run --rm "$remote:{{ branchSlug }}" pylint /app/inverse-canopy
                           """
       }
 
